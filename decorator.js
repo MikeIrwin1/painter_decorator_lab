3fdfd214476a9a2c809a43c49f064f3fb717ebcb
Michael Irwin <mike.irwin1@gmail.com>
2019-01-08 15:51:42 +0000

    decorator can add a can of paint

--- decorator.js
+++ decorator.js
@@ -1,4 +1,8 @@
 const Decorator = function () {
   this.paintStock = [];
 };
+
+Decorator.prototype.addCan = function (can) {
+  this.paintStock.push(can);
+};
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,10 +1,25 @@
 const assert = require('assert');
 const Decorator = require('../decorator');
+const PaintCan = require('../paint_can')
 
 describe('Decorator', function () {
+  let decorator1;
+  let paintCan1;
+
+  beforeEach(function () {
+    decorator1 = new Decorator();
+    paintCan1 = new PaintCan(20);
+  });
+
+
   it('should start with empty paint stock', function () {
-    const decorator1 = new Decorator();
     const actual = decorator1.paintStock;
     assert.deepStrictEqual(actual, []);
   });
+
+  it('should be able to add a can of paint to stock', function () {
+    decorator1.addCan(paintCan1);
+    const actual = decorator1.paintStock.length;
+    assert.strictEqual(actual, 1);
+  })
 });
